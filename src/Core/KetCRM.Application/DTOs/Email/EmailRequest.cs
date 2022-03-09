768f3919734246314273f66eb4cce889b3c82359
Judori <steblevvlad037@gmail.com>
2022-03-09 15:10:55 +0300

    Добавлен EmailRequest

---
+++ src/Core/KetCRM.Application/DTOs/Email/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Application.DTOs.Email
+{
+    public interface EmailRequest
+    {
+        Task SendAsync(EmailRequest request);
+    }
+}
