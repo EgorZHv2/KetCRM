f6d62f766fbf1b73fbace896b4767021442540dd
Judori <steblevvlad037@gmail.com>
2022-03-09 14:31:07 +0300

    Добавлен RefreshToken

---
+++ src/Core/KetCRM.Application/Interfaces/Identity/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using KetCRM.Application.Interfaces.Wrappers;
+
+namespace KetCRM.Application.Interfaces.Identity
+{
+    public class IAccountService
+    {
+        Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+        Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+        Task<Response<string>> ConfirmEmailAsync(string userId, string code);
+        Task ForgotPassword(ForgotPasswordRequest model, string origin);
+        Task<Response<string>> ResetPassword(ResetPasswordRequest model);
+    }
+}
