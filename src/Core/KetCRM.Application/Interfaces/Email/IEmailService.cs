e17f978f1a0771579e4a3b6fd87d462d465d7e49
Judori <steblevvlad037@gmail.com>
2022-03-09 15:21:47 +0300

    Добавлен IEmailSerive

---
+++ src/Core/KetCRM.Application/Interfaces/Email/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using KetCRM.Application.DTOs.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Application.Interfaces.Email
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailRequest request);
+    }
+}
