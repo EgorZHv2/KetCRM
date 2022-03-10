2a6c253a29ef48976a227c137c3b34801d1f3e5c
Judori <steblevvlad037@gmail.com>
2022-03-10 14:42:19 +0300

    Инициализация

---
+++ src/Infrastructure/KetCRM.Infrastructure.Identity/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using KetCRM.Infrastructure.Identity.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Infrastructure.Identity
+{
+    public class DbInitializer
+    {
+        public static void Initialize(IdentityContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
