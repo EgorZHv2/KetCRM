2abcd8858d9d1bc28ca2d68a19d44814d5551b98
Judori <steblevvlad037@gmail.com>
2022-03-10 13:50:27 +0300

    Реализация DateTime

--- src/WebApi/KetCRM.WebApi/Program.cs
+++ src/WebApi/KetCRM.WebApi/Program.cs
@@ -1,7 +1,14 @@
+using KetCRM.Infrastructure.Identity;
+using KetCRM.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
 
 // Add services to the container.
 
+builder.Services.AddIdentityInfrastructure(config);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,12 +16,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
