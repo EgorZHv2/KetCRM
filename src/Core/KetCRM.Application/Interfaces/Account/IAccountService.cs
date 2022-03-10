a05b3d164457b82a970ceb214afbba4838a40780
Judori <steblevvlad037@gmail.com>
2022-03-10 13:49:40 +0300

    -мусор

---
+++ src/Core/KetCRM.Application/Interfaces/Account/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using KetCRM.Application.DTOs.Account;
+using KetCRM.Application.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Application.Interfaces.Account
+{
+    public interface IAccountService
+    {
+        Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+        Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+    }
+}
