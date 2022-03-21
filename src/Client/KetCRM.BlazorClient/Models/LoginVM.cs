459a69457d57ca181838370bb999300384c39ff6
Владислав Стеблев <v.steblev02@gmail.com>
2022-03-21 22:14:04 +0300

    Обновил авторизацию

--- src/Client/KetCRM.BlazorClient/Models/LoginVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KetCRM.BlazorClient.Models
-{
-    public class LoginVM
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- src/Client/KetCRM.BlazorClient/Models/UserListVM.cs
+++
@@ -1,9 +0,0 @@
-﻿using KetCRM.Application.DTOs.Account;
-
-namespace KetCRM.BlazorClient.Models
-{
-    public class UserListVM
-    {
-        public IList<UserListDto> Lists { get; set; } = new List<UserListDto>();
-    }
-}
