40a3376a90a1bf3bcaf879d5158c5cc8e972ba1a
Егор <egor.zholobov@yandex.ru>
2022-03-23 13:05:15 +0300

    Написал команду по добавлению персоны. Приступаю к её обработчику

--- src/Core/KetCRM.Application/PersonBl/Commands/CreatePerson/CreatePersonCommand.cs
+++ src/Core/KetCRM.Application/PersonBl/Commands/CreatePerson/CreatePersonCommand.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using MediatR;
 
 namespace KetCRM.Application.PersonBl.Commands.CreatePerson;
 
-internal class CreatePersonCommand
+internal class CreatePersonCommand:IRequest<int>
 {
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Surname { get; set; }
+    [Required]
+    public string Patronymic { get; set; }
 }
--- src/Core/KetCRM.Application/PersonBl/Commands/CreatePerson/CreatePersonCommandHandler.cs
+++ src/Core/KetCRM.Application/PersonBl/Commands/CreatePerson/CreatePersonCommandHandler.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 
 namespace KetCRM.Application.PersonBl.Commands.CreatePerson;
 
 internal class CreatePersonCommandHandler
-{
+{                 
 }
